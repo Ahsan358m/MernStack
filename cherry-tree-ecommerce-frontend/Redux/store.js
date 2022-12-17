7a96d93f564fcd594bf2506ed3f59336c87a536c
Ahsan358m <ahsan358m@gmail.com>
2022-12-17 19:45:45 +0500

    initial

---
+++ cherry-tree-ecommerce-frontend/Redux/store.js
@@ -0,0 +1,17 @@
+import { combineReducers, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+import cartItems from './Reducers/cartItem'
+
+const reducers = combineReducers({
+    cartItems: cartItems
+})
+
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+)
+
+export default store;
